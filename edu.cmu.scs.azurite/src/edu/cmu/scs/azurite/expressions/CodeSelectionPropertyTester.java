a0d890488d22fe11269ff987289e499665090ef6
YoungSeok Yoon <youngseok@cs.cmu.edu>
2013-07-01 22:30:46 -0400

    Enable/Disable commands that work on the selected code.

---
+++ edu.cmu.scs.azurite/src/edu/cmu/scs/azurite/expressions/CodeSelectionPropertyTester.java
@@ -0,0 +1,48 @@
+package edu.cmu.scs.azurite.expressions;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+
+public class CodeSelectionPropertyTester extends PropertyTester {
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+		switch (property) {
+
+		case "codeSelected": {
+			IEditorPart editorPart = (IEditorPart) receiver;
+
+			if (editorPart instanceof AbstractTextEditor) {
+				// check if there is text selection
+				IEditorSite iEditorSite = editorPart.getEditorSite();
+				if (iEditorSite != null) {
+					ISelectionProvider selectionProvider = iEditorSite
+							.getSelectionProvider();
+					if (selectionProvider != null) {
+						ISelection iSelection = selectionProvider
+								.getSelection();
+						if (iSelection instanceof ITextSelection) {
+							ITextSelection textSelection = (ITextSelection) iSelection;
+							return !textSelection.isEmpty() && textSelection.getLength() > 0;
+						}
+					}
+				}
+			}
+
+			return false;
+		}
+
+		default:
+			break;
+		}
+
+		return false;
+	}
+
+}
