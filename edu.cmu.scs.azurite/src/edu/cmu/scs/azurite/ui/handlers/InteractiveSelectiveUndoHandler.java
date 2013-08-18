841835685139f973611b85bb7ea8814e71c64a1d
YoungSeok Yoon <youngseok@cs.cmu.edu>
2013-08-18 19:50:39 -0400

    Added a command/handler for interactive selective undo.

---
+++ edu.cmu.scs.azurite/src/edu/cmu/scs/azurite/ui/handlers/InteractiveSelectiveUndoHandler.java
@@ -0,0 +1,15 @@
+package edu.cmu.scs.azurite.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class InteractiveSelectiveUndoHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO implement the handler.
+		return null;
+	}
+
+}
