d9cd99f009800d1a8d26dcc2aa59750e8333e365
YoungSeok Yoon <youngseok@cs.cmu.edu>
2013-11-12 22:01:07 -0500

    buster test config

---
+++ edu.cmu.scs.azurite/html/scripts/test/buster.js
@@ -0,0 +1,16 @@
+var config = module.exports;
+
+config["My tests"] = {
+  rootPath: "../",
+  environment: "browser",
+  sources: [
+    "azurite.js",
+    "d3.v3.js",
+    "jquery-1.8.2.js",
+    "jquery.tipsy.js",
+    "timeline.js"
+  ],
+  tests: [
+    "test/*-test.js"
+  ]
+}
