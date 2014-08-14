12bee00059eb9cb510a55cbb01f4595882ee69b1
YoungSeok Yoon <youngseok@cs.cmu.edu>
2014-08-14 17:30:53 -0400

    Display an error message when the review command is invoked without any changes to be reviewed.

--- edu.cmu.scs.azurite/src/edu/cmu/scs/azurite/ui/handlers/LaunchReviewHandler.java
+++ edu.cmu.scs.azurite/src/edu/cmu/scs/azurite/ui/handlers/LaunchReviewHandler.java
@@ -3,12 +3,15 @@ package edu.cmu.scs.azurite.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
+import edu.cmu.scs.azurite.model.RuntimeHistoryManager;
 import edu.cmu.scs.azurite.views.ReviewViewPart;
 
 public class LaunchReviewHandler extends AbstractHandler {
@@ -18,6 +21,14 @@ public class LaunchReviewHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		if (RuntimeHistoryManager.getInstance().getEntireHistory().isEmpty()) {
+			MessageDialog.openInformation(
+					Display.getDefault().getActiveShell(),
+					"Azurite - Review",
+					"There are no chagnes to be reviewed.");
+			return null;
+		}
+		
 		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 		if (window != null) {
 			IWorkbenchPage page = window.getActivePage();
