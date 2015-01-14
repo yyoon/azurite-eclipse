6aa626c4e5dd13aa6a92ed2072660d513bc35e92
YoungSeok Yoon <youngseok@cs.cmu.edu>
2015-01-14 13:35:02 -0500

    Apply correct styles for the type level boxes.

--- edu.cmu.scs.azurite/html/scripts/timeline.js
+++ edu.cmu.scs.azurite/html/scripts/timeline.js
@@ -90,7 +90,7 @@ var MIN_SCROLL_THUMB_SIZE = 30;
 
 
 var NUM_COLLAPSE_LEVELS = 3;
-var COLLAPSE_LEVEL_INITIALS = ['R', 'P', 'M', 'C'];
+var COLLAPSE_LEVEL_INITIALS = ['R', 'P', 'M', 'T'];
 var COLLAPSE_LEVEL_THRESHOLDS = [3.1, 1.5, 0.7, 0.4, 0.1];
 
 
@@ -1578,7 +1578,7 @@ window.onload = function() {
 
 	$collapseButton.contextmenu({
 		menu: [
-			{ title: "<strong>C</strong>lass Level", cmd: 2 },
+			{ title: "<strong>T</strong>ype Level", cmd: 2 },
 			{ title: "<strong>M</strong>ethod Level", cmd: 1 },
 			{ title: "<strong>P</strong>arse Level", cmd: 0 },
 			{ title: "<strong>R</strong>aw Level", cmd: -1 },
